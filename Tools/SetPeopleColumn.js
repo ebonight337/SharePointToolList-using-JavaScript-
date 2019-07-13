899d030d35140d85d9700290c8cde6d9ebbb23e6
ebonight337 <senndayuuki337@yahoo.co.jp>
2019-07-13 20:58:52 +0900

    Create SetPeopleColumn.js

---
+++ Tools/SetPeopleColumn.js
@@ -0,0 +1,11 @@
+<script
+  src="https://code.jquery.com/jquery-3.4.1.js"
+  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
+  crossorigin="anonymous">
+</script>
+<!-- HTMLモデル -->
+<!--<input type="button" name="test" onclick="expandHtml();" value="click!"> デバッグ用-->
+<div id="modalview"></div>
+
+<script>
+"use strict";
