87fe299fe4d843c5fec1a15a04a3521f29b365e2
ebonight337 <senndayuuki337@yahoo.co.jp>
2019-07-19 13:44:27 +0900

    Update GetUserProperties.js

--- Function/GetUserProperties.js
+++ Function/GetUserProperties.js
@@ -1,10 +1,3 @@
-<script
-  src="https://code.jquery.com/jquery-3.4.1.js"
-  integrity="sha256-WpOohJOqMqqyKL9FccASB9O0KwACQJpFTUBLTYOVvVU="
-  crossorigin="anonymous">
-</script>
-
-<script>
 "use strict";
 
 function getUserProperties(){
